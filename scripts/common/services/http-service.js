fbbf7e17f85d86d9235276843918ab9205e82501
dul-nik <dulnik@yandex.ru>
2019-03-25 21:44:13 +0300

    Adds absolute URL

--- scripts/common/services/http-service.js
+++ scripts/common/services/http-service.js
@@ -1,10 +1,12 @@
+const API_URL = 'http://localhost:3000/phones';
+
 const HttpService = {
     sendRequest(url, {
         errorCallback,
         successCallback
     }) {
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
+        xhr.open('GET', API_URL + url);
         xhr.send();
 
         xhr.onload = () => {
